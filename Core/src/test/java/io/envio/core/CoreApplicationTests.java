0d508973d5abaadb4814131158e9e943dd7e6504
wjsrlwjd <wjsrlwjd@kookmin.ac.kr>
2026-05-04 17:17:10 +0900

    ♻️ refactor: 사용자 관련 클래스에 공백 추가
    - 코드 가독성을 높이기 위해 사용자 관련 클래스에 공백 추가
    - 불필요한 변경 없이 코드 구조 개선
    
    Ref: #S14P31A209-33

--- Core/src/test/java/io/envio/core/CoreApplicationTests.java
+++ Core/src/test/java/io/envio/core/CoreApplicationTests.java
@@ -1,9 +1,8 @@
 package io.envio.core;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CoreApplicationTests {
 
 	@Test
