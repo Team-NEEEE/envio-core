0cc1a0f4f17b43b5777f31f505ffa44af8ac2ee0
mingy1206 <mingy837@gmail.com>
2026-05-15 16:48:27 +0900

    ✨ feat: 프로젝트 연동 기능 추가
    - CLI 프로젝트 연동을 위한 메서드 추가
    - 프로젝트 링크 요청 및 응답 DTO 구성
    - GitHub 리포지토리 접근 권한 검증 로직 구현
    
    Ref: #S14P31A209-57

--- Core/src/main/java/io/envio/core/domain/project/service/facade/CliProjectFacadeService.java
+++ Core/src/main/java/io/envio/core/domain/project/service/facade/CliProjectFacadeService.java
@@ -1,13 +1,17 @@
 package io.envio.core.domain.project.service.facade;
 
 import io.envio.core.domain.project.dto.request.ProjectCreateReqDto;
+import io.envio.core.domain.project.dto.request.ProjectLinkReqDto;
 import io.envio.core.domain.project.dto.request.ProjectWrappedKeySaveReqDto;
 import io.envio.core.domain.project.dto.response.ProjectCreateResDto;
+import io.envio.core.domain.project.dto.response.ProjectLinkResDto;
 import io.envio.core.domain.project.dto.response.ProjectWrappedKeySaveResDto;
 
 public interface CliProjectFacadeService {
 
 	ProjectCreateResDto createProject(ProjectCreateReqDto reqDto);
+
+	ProjectLinkResDto linkProject(ProjectLinkReqDto reqDto);
 
 	ProjectWrappedKeySaveResDto saveWrappedKeys(Long projectId, ProjectWrappedKeySaveReqDto reqDto);
 }
