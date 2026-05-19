5614fcb1d09e4426e32e1651f75b79bbe98892cf
wjsrlwjd <wjsrlwjd@kookmin.ac.kr>
2026-05-19 11:07:01 +0900

    ✨ feat: 프로젝트 히스토리 조회 기능 추가
    - 특정 프로젝트의 버전 히스토리를 조회하는 API 추가
    - ProjectHistoryResDto에 encryptedEnvironment 필드 추가
    - ProjectFacadeService에 새로운 메서드 정의 및 구현
    
    Ref: #S14P31A209-77

--- Core/src/main/java/io/envio/core/domain/project/controller/CliProjectController.java
+++ Core/src/main/java/io/envio/core/domain/project/controller/CliProjectController.java
@@ -1,6 +1,10 @@
 package io.envio.core.domain.project.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,15 +12,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.envio.core.common.response.BaseResponse;
+import io.envio.core.common.security.jwt.JwtClaims;
 import io.envio.core.common.util.ResponseUtils;
 import io.envio.core.domain.project.dto.request.ProjectCreateReqDto;
 import io.envio.core.domain.project.dto.request.ProjectLinkReqDto;
 import io.envio.core.domain.project.dto.request.ProjectWrappedKeySaveReqDto;
 import io.envio.core.domain.project.dto.response.ProjectCreateResDto;
+import io.envio.core.domain.project.dto.response.ProjectHistoryResDto;
 import io.envio.core.domain.project.dto.response.ProjectLinkResDto;
 import io.envio.core.domain.project.dto.response.ProjectWrappedKeySaveResDto;
 import io.envio.core.domain.project.service.facade.CliProjectFacadeService;
 
+import io.envio.core.domain.project.service.facade.ProjectFacadeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -27,6 +34,8 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class CliProjectController {
+
+	private final ProjectFacadeService projectFacadeService;
 
 	private final CliProjectFacadeService cliProjectFacadeService;
 
@@ -64,6 +73,15 @@ public class CliProjectController {
 		@Valid @RequestBody final ProjectWrappedKeySaveReqDto reqDto
 	) {
 		ProjectWrappedKeySaveResDto response = cliProjectFacadeService.saveWrappedKeys(projectId, reqDto);
+		return ResponseUtils.ok(response);
+	}
+
+	@Operation(summary = "프로젝트 히스토리 조회", description = "특정 프로젝트의 버전 히스토리를 조회합니다.")
+	@GetMapping("/api/cli/projects/{projectId}/history")
+	public ResponseEntity<BaseResponse<List<ProjectHistoryResDto>>> getProjectHistory(
+		@PathVariable final Long projectId
+	) {
+		List<ProjectHistoryResDto> response = projectFacadeService.getProjectHistory(projectId);
 		return ResponseUtils.ok(response);
 	}
 }
